a1a3ee3366da66fd14faf3491dfc3c8db1cb03f5
kammic94 <kamil.michalik.94@gmail.com>
2019-04-08 15:57:02 +0200

    Add files via upload

--- www/js/pgb.js
+++ www/js/pgb.js
@@ -5,8 +5,21 @@ document.addEventListener("deviceready",onDeviceReady, false);
 
 
 function onDeviceReady() {
-	console.log(navigator.contacts);
+	navigator.contacts.find(fields, onSuccess, onError, options);
 }
+function onSuccess(contacts) {
+    alert('Found ' + contacts.length + ' contacts.');
+};
 
+function onError(contactError) {
+    alert('onError!');
+};
+
+var options = new ContactFindOptions();
+options.filter="*";
+options.multiple=true;
+options.hasPhoneNumber=true;  
+var fields = ["displayName", "name", "PhoneNumber"];
+navigator.contacts.find(fields, onSuccess, onError, options);
 
 
